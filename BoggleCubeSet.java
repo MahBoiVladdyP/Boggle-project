e4f65d1eaf89ec9a484d9c58abb23691550fe192
Winston Yang <wyang737@users.noreply.github.com>
2017-01-24 16:59:10 -0500

    changed getCubeLetter()

--- BoggleCubeSet.java
+++ BoggleCubeSet.java
@@ -33,21 +33,22 @@ public class BoggleCubeSet {
 		return toReturn;
 	}
 
-	private void reset(){
-		for (int i = 0; i < 16; i ++){
-			checked.set(i, false);
+	public void reset(){
+		checked.clear();
+		for (int i = 0; i < 15; i ++){
+			checked.add(false);
 		}
 	}
 
+	private int counter = -1;
+	
 	public char getCubeLetter(){
 		Random rand = new Random();
 		int y = rand.nextInt(6);
-		while (true){
-			int x = rand.nextInt(16);
-			if (checked.get(x) == false){
-				checked.set(x, true);
-				return rawData.get(x).charAt(y);
-			}
+		counter ++;
+		if (counter == 14){
+			return ' ';
 		}
+		return rawData.get(counter).charAt(y);
 	}
 }
